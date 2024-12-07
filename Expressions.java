c82600d34f4d8c4a42f367e29fba9291ef32996a
arizfaiyaz <arizfaiyazwork@gmail.com>
2024-12-07 22:13:44 +0530

    expressions

---
+++ Expressions.java
@@ -0,0 +1,27 @@
+public class Expressions {
+    public static void main(String[] args) {
+        // Expressions = operands & operators
+        // Operands = values, variables, numvers, quantity
+        // operators = +, -, 8, /, %
+        int friends = 10;
+
+       //  addition
+       //  friends = friends + 1; output = 11
+       
+       //  subtraction
+       //  friends = friends -1;
+
+       //  divide 
+       //  friends = friends / 2;
+
+       //remainder
+       //friends = friends % 3;
+
+       //increment
+       //friends++;
+       
+       // decrement
+       //friends--;
+        System.out.println(friends);
+    }
+}
